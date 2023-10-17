eccfd5d9d55b73ab95ed29840620c56aa2f67c29
Luis Flores <76552948+flores-luis@users.noreply.github.com>
2023-10-17 01:50:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
+from datetime import datetime
+from tabnanny import verbose
 
-# A very simple Flask Hello World app for you to get started with...
+import api.bibleAPI as bib
+import requests
+from api.bibleAPI import getVerse
+from api.currentWeatherAPI import getCurrentWeather
+from api.weatherGeocode import getWeatherGeoCode
+from flask import Flask, render_template
 
-from flask import Flask
+# Directs Flask to find Static files
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
-app = Flask(__name__)
+#dt = datetime.now()
+# currentTime = dt.strftime('%I:%M %p')
+#currentDayOfWeek,currentMonth,currentDay = dt.strftime('%A'),dt.strftime('%B'),dt.strftime('%d')
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
 
+@app.route("/")
+def index():
+    chapter,verse = bib.getVerse()
+    lat,lon = getWeatherGeoCode()
+    temp, city, description, iconURL = getCurrentWeather(lat,lon)
+    return render_template("index.html", 
+                           chapter=chapter,
+                           verse=verse,
+                           temperature = round(temp),
+                           city = city,
+                           description = description,
+                           imagePath = iconURL)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
